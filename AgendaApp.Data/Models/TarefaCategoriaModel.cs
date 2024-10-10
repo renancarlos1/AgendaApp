517b2ba9a54c1ec253f2c3d81798059e52528ed6
renancarlos1 <renancarlos1@yahoo.com.br>
2024-10-10 19:10:21 -0300

    first commit

---
+++ AgendaApp.Data/Models/TarefaCategoriaModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgendaApp.Data.Models
+{
+    /// <summary>
+    /// Modelo de dados para consulta de tarefas por categoria
+    /// </summary>
+    public class TarefaCategoriaModel
+    {
+        public string? Categoria { get; set; }
+        public int QtdTarefas { get; set; }
+    }
+}
+
+
+
